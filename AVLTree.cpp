9e1dfa2dc3f700b0215371d50b897eb04d9f1e04
Zander Little <little.147@wright.edu>
2025-11-18 16:25:47 -0500

    fixed bug causing updateHeight not to use the newly calculated height value.

--- AVLTree.cpp
+++ AVLTree.cpp
@@ -353,6 +353,7 @@ void AVLTree::updateHeight(AVLNode*& node) {
 	} else {
 		height = leftHeight + 1;
 	}
+	// set final height
 	node->setHeight(height);
 }
 int AVLTree::getBalanceFactor(AVLNode*& node) {
